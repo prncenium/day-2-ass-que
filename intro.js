85336028972b671d2a28b0a0e936764823b05f03
prncenium <princecsk666@gmail.com>
2024-11-28 12:17:25 +0530

    again edit

--- intro.js
+++ intro.js
@@ -2,4 +2,8 @@ console.log("RAJA mouli film blahh blahh");
 console.log("1. git branch --list {to get the branch list }");
 console.log("2. git branch <branch name>{to creAte branch }");
 console.log("NOTE: {MAIN BRANCH KI FILES AUTOMATICALLY BAAKI BRANCH MEI AA JTI H , BUT REVERSE NHI HOTA AUTOMATICALLY }");
-console.log("2. git branch <branch name>{to creAte branch }");
+console.log("following steps to take for merging the current with main branch");
+console.log("3. git add . {current created new file added }");
+console.log("4. git commit -m " " ");
+console.log("git push origin <branch name> {branch is on github now} ");
+
